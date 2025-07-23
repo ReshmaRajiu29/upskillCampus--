e1ddd2828b99040db047d66d026cb45a3a6f7857
Reshma Raji <152305825+ReshmaRajiu29@users.noreply.github.com>
2025-07-23 11:50:45 +0530

    Add files via upload

---
+++ Password_Manager.py
@@ -0,0 +1,39 @@
+﻿from prettytable import PrettyTable
+
+x = PrettyTable()
+
+def view():
+    print("")
+    with open("password.txt" , "r") as ff:
+        for i in ff.readlines():
+            data = i.strip()
+            acc , user , pas = data.split("|")
+            x.add_row([acc, user,pas])
+            
+    x.field_names = ["Account", "Username", "Password"]
+    print(x)
+    x.clear_rows()
+    
+def add():
+    account = input("\nEnter Account Name: ")
+    user_a = input("\nEnter Username: ")
+    pass_a = input("\nEnter Password: ")
+
+    with open("password.txt" , 'a') as qq:
+        qq.write(account +" | "+ user_a + " | " + pass_a +"\n")
+
+    print(f"\n--- Successfully add a {account} Account to your Password Manager --- \n")
+
+while True:
+    mode = input("\nWould you like to add new password or view passwords,\nplease enter ones(add , view), press q to quit()? ")
+    if mode == 'q':
+        break
+    elif mode == 'add':
+        add()
+    elif mode == 'view':
+        view()
+    else: 
+        print("\n--Invalid mode. try again--")
+        continue
+
+# Made By Yasin Rezvani
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# upskillCampus--
+# Password_Manager
+Simple CLI password manager in terminal to manage your account password.
+# Install the requirements
+First you should install the requirements:
+```
+pip install prettytable
+```
+# Demo :tada:
+### Add
+![demo_add](https://user-images.githubusercontent.com/77124662/129404654-93103d38-de03-41cf-96ef-516726236393.PNG)
+### View
+![demo_view](https://user-images.githubusercontent.com/77124662/132183084-787e7c6e-c585-481a-80a0-6beeeedf3bc2.PNG)
+
+
